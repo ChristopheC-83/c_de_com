743a3a4fe8b5e783a37a968b676c7d39adbdf64d
kikisan <kiketdule@gmail.com>
2024-07-21 11:01:26 +0200

    hero ok

--- app/(pages)/home/components/EarthComponent.jsx
+++ app/(pages)/home/components/EarthComponent.jsx
@@ -3,16 +3,16 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export default function EarthComponent() {
   return (
-    <div className="container">
+    <div className="container max-md:overflow-hidden max-md:midFlex max-md:px-0">
       <AspectRatio ratio={2 / 1}>
-        <div className="flex w-full h-full ">
-          <div className="flex items-start justify-start h-full basis-2/5">
-            <div className="relative w-full scale-125 translate-x-[12.5%]">
+        <div className="flex w-full h-full max-md:flexMid max-md:mx-auto">
+          <div className="flex items-start justify-start h-full basis-2/5 max-md:absolute max-md:inset-0 ">
+            <div className="relative w-full md:scale-125 md:translate-x-[12.5%] min-w-[300px] max-md:w-[80vw] max-md:mx-auto">
               <Earth />
             </div>
           </div>
-          <div className="z-20 flex flex-col justify-center basis-3/5">
-            <div className="flex-col px-10 py-6 text-white rounded-full flexMid w-fit bg-clip box-shadow">
+          <div className="z-20 mx-0 flexMid max-md:mx-auto max-md:flex-col max-md:basis-3/5 max-md:w-screen">
+            <div className="flex-col px-8 py-4 lg:px-10 lg:py-6 text-white rounded-full flexMid w-fit bg-clip box-shadow max-sm:scale-[0.7] max-md:scale-[0.80] max-lg:scale-[0.95] text-nowrap max-md:mx-0">
                 <h1 className="w-fit">Votre Site Internet</h1>
                 <h3 className="mb-2 w-fit">Votre espace de communication</h3>
             </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,21 @@
-"use client"
+"use client";
 
 import { useEffect } from "react";
 import HomePage from "./(pages)/home/page";
 
 export default function Home() {
+  // A décommenter au moment du déploiement
 
-  useEffect(() => {
-    const themes = ['sunshine', 'happy', 'deep', 'dark', 'gray', 'salad'];
+  // useEffect(() => {
+  //   const themes = ['sunshine', 'happy', 'deep', 'dark', 'gray', 'salad'];
 
-    function applyRandomTheme() {
-      const randomTheme = themes[Math.floor(Math.random() * themes.length)];
-      document.documentElement.setAttribute('data-theme', randomTheme);
-    }
+  //   function applyRandomTheme() {
+  //     const randomTheme = themes[Math.floor(Math.random() * themes.length)];
+  //     document.documentElement.setAttribute('data-theme', randomTheme);
+  //   }
 
-    applyRandomTheme();
-  }, []);
+  //   applyRandomTheme();
+  // }, []);
 
   return (
     <>
