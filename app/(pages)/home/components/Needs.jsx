80cf00f75375803cb6a0ff3a70142fe268927ab0
kikisan <kiketdule@gmail.com>
2024-07-23 06:08:31 +0200

    needs

--- app/(pages)/home/components/Needs.jsx
+++ app/(pages)/home/components/Needs.jsx
@@ -3,17 +3,39 @@ import { Separator } from "@/components/ui/separator";
 import { needsItems } from "@/datas/needsItems";
 import Icons from "./_Icons";
 import { Button } from "@/components/ui/button";
+import idea from "@/public/images/frames/idea.jpg";
+import Image from "next/image";
 
 export default function Needs() {
   return (
-    <div className="container px-4 mt-0 overflow-x-hidden xs:px-6 md:px-8">
+    <div className="container px-4 overflow-x-hidden xs:px-6 md:px-8">
       <Separator />
-      <div className="flex">
-        IMAGE D'ILLUSTRATION
-        <h2 className="pb-3 mx-auto text-clip w-fit">
+      <div className="flex items-center justify-between my-6">
+        <div className="basis-1/2 sm:justify-start sm:items-start midFlex">
+          <Image
+            src={idea}
+            alt="christophe c."
+            width={333}
+            height={333}
+            className="object-cover border-2 rounded-full shadow border-ring bg-gradient-to-bl from-teal-400 to-indigo-300 customShadow aspect-square"
+            priority={true}
+          />
+        </div>
+        <h2 className="mx-auto text-clip basis-1/2 ">
           Déterminons vos besoins !
         </h2>
       </div>
+      {/* <div
+        className="flex h-[10vw] max-h-[250px] min-h-[150px] customBorder customShadow my-6 sm:py-5 lg:py-6 rounded-xl relative  bg-no-repeat bg-contain bg-left-center overflow-hidden"
+        style={{ backgroundImage: "url('/images/frames/idea.jpg')" }}
+      >
+        <div className="absolute inset-0 z-20 bg-gradient-to-l from-neutral-100/90 from-40% to-neutral-100/0">
+          {" "}
+        </div>
+        <h2 className="z-40 pb-3 ml-auto mr-4 w-fit text-neutral-900/80 textShadowClip">
+          Déterminons vos besoins !
+        </h2>
+      </div> */}
       <div className="flex justify-between w-full gap-4 pb-4 max-md:flex-col">
         {needsItems.map((item) => (
           <div
@@ -21,14 +43,21 @@ export default function Needs() {
             className="flex flex-col items-center justify-between w-[49%] p-2 customBorder customShadow rounded-xl xs:p-3 md:p-4 max-md:w-full"
           >
             <div className="flex items-end w-full mt-2 mb-5 rounded-full justify-evenly">
-                <Icons size="text-2xl"> {item.icon1} </Icons>
-                <Icons size="text-4xl"> {item.icon2} </Icons>
-                <Icons size="text-2xl"> {item.icon3} </Icons>
-              
+              <Icons size="text-2xl"> {item.icon1} </Icons>
+              <Icons size="text-4xl"> {item.icon2} </Icons>
+              <Icons size="text-2xl"> {item.icon3} </Icons>
             </div>
-            <p className="mb-5" dangerouslySetInnerHTML={{ __html: item.description }}></p>
+            <p
+              className="mb-5"
+              dangerouslySetInnerHTML={{ __html: item.description }}
+            ></p>
 
-            <Button href={item.href} className="p-4 mb-3 text-white duration-200 rounded-full bg-clip hover:opacity-80 "><h4>{item.text_btn}</h4></Button>
+            <Button
+              href={item.href}
+              className="p-4 mb-3 text-white duration-200 rounded-full bg-clip hover:opacity-80 "
+            >
+              <h4>{item.text_btn}</h4>
+            </Button>
           </div>
         ))}
       </div>
