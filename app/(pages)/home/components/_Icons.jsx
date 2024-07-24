749545435784876d830a5ecd200e9cfd8884af90
kikisan <kiketdule@gmail.com>
2024-07-24 17:39:31 +0200

    modif couleur icones si dark

--- app/(pages)/home/components/_Icons.jsx
+++ app/(pages)/home/components/_Icons.jsx
@@ -1,6 +1,20 @@
-export default function Icons({ children, size="text-xl"}) {
+import { useEffect, useState } from "react";
+
+export default function Icons({ children, size = "text-xl" }) {
+  // On récupére le theme pour changer couleur des icones si besoin
+  const [theme, setTheme] = useState("");
+
+  useEffect(() => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    setTheme(currentTheme || "default");
+  }, [theme]);
+
   return (
-    <div className={`p-3 ${size} rounded-full filterShadow bg-clip flip_hover`}>
+    <div
+      className={`p-3 ${size} rounded-full filterShadow bg-clip flip_hover ${
+        theme === "dark" ? "text-white" : ""
+      }`}
+    >
       {children}
     </div>
   );
