93d769e1f135ae440f32895c936f30cf27547273
kikisan <kiketdule@gmail.com>
2024-07-27 18:23:09 +0200

    dynamic ok

--- app/(pages)/solutions/components/Dynamic.jsx
+++ app/(pages)/solutions/components/Dynamic.jsx
@@ -1,16 +1,31 @@
 
-
+import { solutions_static } from "@/datas/solutions_static";
+import { solutions_global } from "@/datas/solutions_global";
+import { solutions_dynamic } from "@/datas/solutions_dynamic";
+import Frame from "./Frame";
 
  export default function Dynamic() {
 
+  let modulo;
+  let modulo_calcul = solutions_global.length + solutions_static.length;
+  console.log("modulo_calcul", modulo_calcul);
 
+  if(modulo_calcul % 2 === 0){
+    modulo = 1;
+  } else{
+    modulo = 0;
+  }
 
 return (
-    <div className={`container`} id="dynamic"> 
-
-dynamic
-
-    </div>
+  <div className={`container`} id="dynamic">
+  <div className={`w-fit mx-auto`}>
+    <h1 className={`text-clip w-fit mx-auto my-10`}>Un site dynamique ?</h1>
+    
+  </div>
+  {solutions_dynamic.map((solution, index) => (
+    <Frame key={index} solution={solution} index={index} modulo={modulo}/>
+  ))}
+</div>
 
   );
 
---
+++ datas/solutions_dynamic.js
@@ -0,0 +1,31 @@
+export const solutions_dynamic = [
+    {
+      id: 10,
+      title: "Exposer vos propositions !",
+      text: "Que ce soit matériel (boutique, créations artistiques) ou immatériels (formation, articles) , vous avez besoin de montrer à vos clients ce que vous avez à leur offrir. <br/> Un site dynamique vous permettra de mettre en avant vos propositions, de les mettre à jour facilement et de les adapter à vos clients.<br/> Il est, grâce à vous, toujours à jour !.",
+      icon: "/images/solutions/icons/creation.png",
+      illustration: "/images/solutions/illustrations/merchandising.jpg",
+    },
+    {
+      id: 20,
+      title: "Adaptabilité",
+      text: "A travers sa connexion, ses droits d'accès, ses bases de données, un site dynamique est capable de s'adapter à vos clients, à vos collaborateurs. <br/> Il peut être personnalisé, proposer des contenus, des droits différents selon les profils et selon vos envies et besoins..",
+      icon: "/images/solutions/icons/difference.png",
+      illustration: "/images/solutions/illustrations/personnes.jpg",
+    },
+    {
+      id: 30,
+      title: "Administration",
+      text: "Gérez vos articles, vos collaborateurs/modérateurs et leurs droits... un site dynamique propose un zone d'administration ou un second site séparé pour gérer tout cela ! <br/> Vous pouvez ainsi mettre à jour vos contenus, vos produits, vos tarifs, vos promotions, vos actualités... en toute autonomie.",
+      icon: "/images/solutions/icons/administratif.png",
+      illustration: "/images/solutions/illustrations/dashboard.jpg",
+    },
+    {
+      id: 40,
+      title: "Le prix",
+      text: "Plus de travail, plus de temps, plus de fonctionnalités... un site dynamique est plus cher à l'achat. <br/> Mais il est aussi plus rentable, plus efficace, plus évolutif, plus adapté à vos besoins et à ceux de vos clients et propects. <br/> Il est un investissement sur le long terme.",
+      icon: "/images/solutions/icons/price.png",
+      illustration: "/images/solutions/illustrations/billets.jpg",
+    },
+  ];
+  
