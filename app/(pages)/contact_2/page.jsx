c137f2af1b69d79d34abe146be93cccab688166f
kikisan <kiketdule@gmail.com>
2024-08-14 22:27:11 +0200

    formulaire contact ok

---
+++ app/(pages)/contact_2/page.jsx
@@ -0,0 +1,185 @@
+"use client";
+import FrameSlideFromBottom from "@/components/commons/FrameSlideFromBottom";
+import useThemeStore from "@/store/ThemeStore";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+export default function Contact_2() {
+  const { theme } = useThemeStore();
+  const router = useRouter();
+  const themesWithWhiteText = ["dark", "gray", "deep"];
+  const [btnBlocked, setBtnBlocked] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  async function onSubmitHandler(data) {
+    setBtnBlocked(true);
+    try {
+      const response = await axios.post("/api/send_mail", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 200) {
+        toast.success("Votre message a bien été envoyé !");
+        // router.push("/");
+      } else {
+        throw new Error("Erreur lors de l'envoi du message");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Une erreur est survenue lors de l'envoi");
+    } finally {
+      setTimeout(() => setBtnBlocked(false), 3000);
+    }
+  }
+
+  return (
+    <FrameSlideFromBottom>
+      <div className={`container min-h-svh`}>
+        <h1 className={`text-clip text-center my-6`}>Formulaire de contact 2</h1>
+        <div className="flex flex-col w-full h-auto mt-8 p-1.5 xs:p-2 sm:p-3 md:p-4 customShadow rounded-xl customBorder gap-4 bg-gradient-to-tr from-neutral-200 to-neutral-100">
+          <form onSubmit={handleSubmit(onSubmitHandler)}>
+            <input
+              type="hidden"
+              {...register("email_recep", { required: true })}
+              value="christophe.chiappetta@gmail.com"
+            />
+
+            <div className="flex w-full max-md:flex-col md:gap-x-4">
+              {/* prénom */}
+              <label
+                htmlFor="prenom"
+                className="relative block my-3 border border-gray-400 shadow-sm focus-within:border-neutral-600 focus-within:ring-1 focus-within:ring-neutral-600 rounded-xl grow"
+              >
+                <input
+                  type="text"
+                  id="prenom"
+                  className="w-full p-4 placeholder-transparent bg-transparent border-none peer focus:border-transparent focus:outline-none focus:ring placeholder-gray-50 rounded-xl"
+                  placeholder="prenom"
+                  {...register("prenom", { required: true })}
+                />
+                <span
+                  className={`pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-clip p-2 rounded-full text-xs  transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs w-28 text-center ${
+                    themesWithWhiteText.includes(theme)
+                      ? "text-white"
+                      : "text-gray-700"
+                  }`}
+                >
+                  <b>Prénom</b>
+                </span>
+              </label>
+              {/* nom */}
+              <label
+                htmlFor="nom"
+                className="relative block my-3 border border-gray-400 shadow-sm rounded-xl focus-within:border-neutral-600 focus-within:ring-1 focus-within:ring-neutral-600 grow"
+              >
+                <input
+                  type="text"
+                  id="nom"
+                  className="w-full p-4 placeholder-transparent bg-transparent border-none peer focus:border-transparent focus:outline-none focus:ring placeholder-gray-50 rounded-xl"
+                  placeholder="nom"
+                  {...register("nom", { required: true })}
+                />
+                <span
+                  className={`pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-clip p-2 rounded-full text-xs  transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs w-28 text-center  ${
+                    themesWithWhiteText.includes(theme)
+                      ? "text-white"
+                      : "text-gray-700"
+                  }`}
+                >
+                  <b>Nom</b>
+                </span>
+              </label>
+            </div>
+
+            {/* email */}
+            <label
+              htmlFor="email"
+              className="relative block my-3 border border-gray-400 shadow-sm rounded-xl focus-within:border-neutral-600 focus-within:ring-1 focus-within:ring-neutral-600"
+            >
+              <input
+                type="text"
+                id="email"
+                className="w-full p-4 placeholder-transparent bg-transparent border-none peer focus:border-transparent focus:outline-none focus:ring placeholder-gray-50 rounded-xl"
+                placeholder="email"
+                {...register("email", { required: true })}
+              />
+              <span
+                className={`pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-clip p-2 rounded-full text-xs  transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs w-28 text-center  ${
+                  themesWithWhiteText.includes(theme)
+                    ? "text-white"
+                    : "text-gray-700"
+                }`}
+              >
+                <b>Email</b>
+              </span>
+            </label>
+
+            {/* Message */}
+            <label
+              htmlFor="message"
+              className="relative block my-6 border border-gray-400 shadow-sm rounded-xl focus-within:border-neutral-600 focus-within:ring-1 focus-within:ring-neutral-600 "
+            >
+              <textarea
+                type="text"
+                id="message"
+                className="w-full p-4 placeholder-transparent bg-transparent border-none peer focus:border-transparent focus:outline-none focus:ring placeholder-gray-50"
+                rows="10"
+                placeholder="message"
+                {...register("message", { required: true })}
+              />
+              <span
+                className={`pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-clip p-2 rounded-full text-xs transition-all peer-placeholder-shown:top-[30px] peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs  w-28 text-center  ${
+                  themesWithWhiteText.includes(theme)
+                    ? "text-white"
+                    : "text-gray-700"
+                }`}
+              >
+                <b>Message</b>
+              </span>
+            </label>
+
+            <button
+              className={`w-full p-4 mx-auto mt-3 mb-5 duration-200 rounded-full shadow-lg bg-clip hover:opacity-80 ${
+                themesWithWhiteText.includes(theme)
+                  ? "text-white"
+                  : "text-gray-700"
+              }`}
+              disabled={btnBlocked}
+            >
+              <h4 className={`w-full `}>Envoyer</h4>
+            </button>
+          </form>
+        </div>
+
+        <div className="my-8 text-3xl text-center">
+          <br />
+          Vous pouvez également me contacter par mail :
+          <a href="mailto:contact@ducompagnon.fr">
+            {" "}
+            <u>
+              <b>contact@ducompagnon.fr</b>
+            </u>
+          </a>
+          <br />
+          ou par téléphone au :{" "}
+          <a href="tel:0699812296">
+            {" "}
+            <u>
+              <b>06.99.81.22.96</b>
+            </u>
+          </a>
+        </div>
+      </div>
+    </FrameSlideFromBottom>
+  );
+}
