b3b31a366ef892aba9175545e2eb7f3814ea14ce
kikisan <kiketdule@gmail.com>
2024-08-15 18:07:46 +0200

    maj contact + referencement

---
+++ sitemap.js
@@ -0,0 +1,36 @@
+
+ 
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.compagnondecom.fr/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.compagnondecom.fr/solutions',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.compagnondecom.fr/sequences',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.compagnondecom.fr/prices',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.compagnondecom.fr/contactez_moi',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
